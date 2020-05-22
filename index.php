9560b326c8dad0096c3d5eeb15d473454b7bff76
Alexey <a.s.kashtanov@gmail.com>
2020-05-22 13:36:51 +0300

    -

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 
 spl_autoload_register(function ($class_name) {
-    $path = 'application/';
+    $path = './';
     $path .= str_replace('\\', "/", "{$class_name}.php");
     if(file_exists($path)){
         include $path;
     }
 });
+
+
+$categoriesInArray = \models\entity\Category::repository()->find()
+    ->select('*')
+    ->where('id = 1')
+    ->andWhere("name = 'test'")
+    ->asArray()
+    ->all()
+;
